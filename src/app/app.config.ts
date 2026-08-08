a64db66484d68e3a551b589607d741996a4fbdb7
Balemlay0115 <Balemlay0115@gmail.com>
2026-08-08 12:05:07 +0300

    ex 4 complete

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
+    provideAnimations(),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,22 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'instructor',
+    path: 'dashboard',
     loadComponent: () =>
       import('./features/instructor-dashboard/instructor-dashboard.component').then(
         (m) => m.InstructorDashboardComponent,
       ),
   },
   {
-    path: 'dashboard',
+    path: 'enrollments',
     loadComponent: () =>
-      import('./features/student-dashboard/student-dashboard.component').then(
-        (m) => m.StudentDashboardComponent,
+      import('./features/enrollment-list/enrollment-list.component').then(
+        (m) => m.EnrollmentListComponent,
       ),
   },
+
   {
-    path: 'courses/:id',
+    path: 'courses',
     loadComponent: () =>
       import('./features/course-detail/course-detail').then((m) => m.CourseDetailComponent),
   },
@@ -25,11 +26,12 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./features/enrollment-form/enrollment-form').then((m) => m.EnrollmentFormComponent),
   },
+
   {
-    path: 'enrollment-list',
+    path: 'grade-submission',
     loadComponent: () =>
-      import('./features/enrollment-list/enrollment-list.component').then(
-        (m) => m.EnrollmentListComponent,
+      import('./features/grade-submission/grade-submission.component')
+    .then( m => m.GradeSubmissionComponent,
       ),
   },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
