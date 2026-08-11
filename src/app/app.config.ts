501f886cb7035c5866d8ce8b0b2382356807263c
Balemlay0115 <Balemlay0115@gmail.com>
2026-08-11 11:48:52 +0300

    another one

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { withInterceptors, withXsrfConfiguration } from '@angular/common/http';
+import { credentialsInterceptor } from './interceptors/credentials.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
-    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([credentialsInterceptor]),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN', // Cookie name set by .NETserver
+        headerName: 'X-XSRF-TOKEN', // Header expected by .NETserver
+      }),
+    ),
   ],
 };
